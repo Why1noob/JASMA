bb9d293c003321a2dda1fc7910ce59c65808e82b
Why1noob <Why1noob@github.com>
2014-10-25 01:08:30 +0400

    Finally did login and session exparation

--- login.php
+++ login.php
@@ -1,17 +1,18 @@
 <?
 	session_start();
+	$_SESSION['Last_Activity'] = time();
 	$error = "";
     $pass=$_POST['password'];
     if($pass == "")
    {
-    $error = "Please enter the password";
+    $error = "";
    }
    else
    {
    	if($pass == A1b37s86GTR52o0)
     {
-    	$_SESSION['not_logged']=flase;
         header("location: main.php");
+        $_SESSION['logged']=1;
     }
     else
     {
--- session.php
+++ session.php
@@ -1,6 +1,13 @@
 <?
-$isLogged = $_SESSION['not_logged'];
-if(isLogged){
+session_start();
+$_SESSION['Last_Activity'] = time();
+$isLogged = $_SESSION['logged'];
+	if(!isset($isLogged))
+{
 	header('location: index.php');
+}
+if(isset($_SESSION['Last_Activity']) && (time() - $_SESSION['Last_Activity'] > 120)){
+	session_unset();
+	session_destroy();
 }
 ?>
