000f818bc5522e1e9c7ade5bfa9355c6d1fe4e43
Artem <Why1noob@gmail.com>
2014-10-24 14:04:03 +0300

    Very first version of JASMA

---
+++ js/login.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+function checkPass()
+    {
+    var pass = document.login.password.value;
+        if (pass == 'A1b37s86GTR52o0')
+        {
+            window.location = "main.html"
+        }
+    }
